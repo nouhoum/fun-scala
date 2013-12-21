280ef87e849cf21f774744380c2f605080acec57
Nouhoum Traoré <nouhoum.traore@gmail.com>
2013-12-22 00:11:57 +0100

    Tree implementation

---
+++ src/main/scala/fun/scala/datastructures/Tree.scala
@@ -0,0 +1,41 @@
+package fun.scala.datastructures
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+	def size[A](t: Tree[A]): Int = t match {
+		case Leaf(_) => 1
+		case Branch(l, r) => 1 + size(l) + size(r) 
+	}
+
+	def maxOf[A](t: Tree[Int]): Int = t match {
+		case Leaf(v) => v
+		case Branch(l, r) => maxOf(l) max maxOf(r) 
+	}
+
+	def depth[A](t: Tree[A]): Int = t match {
+		case Leaf(_) => 0
+		case Branch(l, r) => 1 + (depth(l) max depth(r))
+	}
+
+	def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+		case Leaf(v) => Leaf(f(v))
+		case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+	}
+
+	def fold[A, B](t: Tree[A])(f: A => B)(g : (B, B) => B): B = t match {
+		case Leaf(v) => f(v)
+		case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))(f)(g) 
+	}
+	
+	def size2[A](t: Tree[A]): Int =
+		fold(t)(a => 1)(_ + _)
+
+
+	def size[A](t: Tree[A]): Int = t match {
+		case Leaf(_) => 1
+		case Branch(l, r) => 1 + size(l) + size(r) 
+	}
+}
