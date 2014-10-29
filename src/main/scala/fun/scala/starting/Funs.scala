1aa8dc01e9c736f0b5128d4f5d7fa25569c622f2
Nouhoum Traoré <nouhoum.traore@gmail.com>
2014-10-29 18:22:39 +0100

    Implement curry, uncurry and compose

---
+++ src/main/scala/fun/scala/starting/Funs.scala
@@ -0,0 +1,12 @@
+package fun.scala.starting
+
+object Funs {
+  def curry[A, B, C](f: (A, B) => C): A => B => C =
+    a => b => f(a, b)
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
+}
